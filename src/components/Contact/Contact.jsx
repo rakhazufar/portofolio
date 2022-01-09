cad122f70113137a85fec378d13e79794224c894
Rakha Zufar <rakhazufar1@gmail.com>
2022-01-09 11:03:45 +0700

    add sosial media in contact section

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 import Button from "../Button/Button";
+import {
+  AiOutlineWhatsApp,
+  AiOutlineInstagram,
+  AiOutlineGithub,
+} from "react-icons/ai";
 
 export default function Contact() {
   return (
@@ -9,12 +14,35 @@ export default function Contact() {
         <p>
           If you intersted to work with me, don't hasitate to send me a massage
         </p>
-        {/* <img src={ContactImage} alt="" /> */}
+        <div className="sosial-media">
+          <a
+            href="https://api.whatsapp.com/send/?phone=6289639143290"
+            className="whatsapp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineWhatsApp />
+          </a>
+          <a
+            className="instagram"
+            href="https://www.instagram.com/rakhazufar/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineInstagram />
+          </a>
+          <a
+            href="https://github.com/rakhazufar"
+            className="github"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <AiOutlineGithub />
+          </a>
+        </div>
       </div>
       <div className="contactForm">
         <form>
-          <h2>Contact Me</h2>
-
           <label htmlFor="name">Name</label>
           <input type="text" id="name" />
           <label htmlFor="email">Email</label>
